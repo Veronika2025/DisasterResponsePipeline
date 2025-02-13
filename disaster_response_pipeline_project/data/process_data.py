97997de4dcdc6f57936816587ed805c8c37c0241
Veronika2025 <veronika.hartl@bmw.de>
2025-02-13 16:17:59 +0100

    Update process_data.py
    update code

--- disaster_response_pipeline_project/data/process_data.py
+++ disaster_response_pipeline_project/data/process_data.py
@@ -1,16 +1,69 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    Load messages and categories data from CSV files and merges them.
+
+    Args:
+    messages_filepath (str): The file path to the CSV file containing messages.
+    categories_filepath (str): The file path to the CSV file containing categories.
+
+    Returns:
+    DataFrame: A pandas DataFrame containing the merged data from messages and categories
+    """
+    messages = pd.read_csv(messages_filepath)
+    categories_df = pd.read_csv(categories_filepath)
+    df = messages.merge(categories_df, how="outer",on=["id"])
+    
+    return df
+
 
 
 def clean_data(df):
-    pass
+    """
+    Clean and transform the input DataFrame containing disaster response data.
+
+    
+    Parameters:
+    df (DataFrame): A pandas DataFrame containing disaster response messages and categories.
+
+    Returns:
+    DataFrame: A cleaned pandas DataFrame with the original data and individual category columns,
+                where each category column contains binary values.
+    """
+      
+    
+    # create a dataframe of the 36 individual category columns
+     categories_df = df["categories"].str.split(";", expand = True)
+     row = categories_df.values[0]
+
+    # use this row to extract a list of new column names for categories.
+     category_colnames = list(map(lambda x: x.split('-')[0], row))
+    # rename the columns of `categories`
+     categories_df.columns = category_colnames
+    
+     for column in categories_df:
+    # set each value to be the last character of the string
+         categories_df[column] = categories_df[column].astype(str).str[-1]
+    # convert column from string to numeric
+         categories_df[column] = categories_df[column].astype(int)
+    # drop the original categories column from `df`
+     df = df.drop("categories", axis=1)
+    # concatenate the original dataframe with the new `categories` dataframe
+     df = pd.concat([df, categories_df],axis=1)
+    # drop duplicates
+     df = df.drop_duplicates()
+        
+     return df
 
 
 def save_data(df, database_filename):
-    pass  
+    # save a dataframe to sqlite database
+    print('Save {} to {} database: '.format(df, database_filename))
+    engine = create_engine('sqlite:///{}'.format(database_filename))
+    df.to_sql('DisasterResponse', engine, index=False)
 
 
 def main():
